878af278bece8ba9f5fdb8349b9e4da2479b1f8d
stella <stella.choi@hyperisland.se>
2022-10-03 19:54:05 -0400

    add google maps

--- pages/bike.tsx
+++ pages/bike.tsx
@@ -1,14 +1,46 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import styled from 'styled-components'
 
+const Location = styled.input`
+    border: 2px solid #BDFF00;
+    border-radius: 100px;
+    background: transparent;
+    padding: 7px 10px;
+    font-size: 17px;
+    width: 350px;
+    font-style: italic;
+    text-align: center;
+`
+
+const Transit = styled.input`
+    border: 2px solid #BDFF00;
+    border-radius: 100px;
+    background: transparent;
+    padding: 5px 10px;
+    font-size: 17px;
+    width: 100px;
+    font-style: italic;
+    text-align: center;
+`
+
+// const Find = styled.button`
+//     font-size: 17px;
+//     background: #FF0099;
+//     border: none;
+//     border-radius: 100px;
+//     padding: 10px;
+// `
 
 const Bike = () => {
     return (
         <main className={styles.main}>
             <Image src="/bike.png" alt="bike" width={175} height={100} />
-
-            <div>hi</div>
+            <Location type="text" placeholder="Current location" />
+            Desired transit time
+            <span><Transit type="value" placeholder="00" /> mins</span>
+            {/* <Find>FIND BEATS AND BEERS</Find> */}
         </main>
     )
 }
