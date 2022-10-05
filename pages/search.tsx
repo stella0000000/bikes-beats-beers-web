1aafeff8646863985ff17cd2e31ba8e46c03d8ed
stella <stella.choi@hyperisland.se>
2022-10-05 12:07:06 -0400

    nothing is broken

--- pages/search.tsx
+++ pages/search.tsx
@@ -5,33 +5,11 @@ import styles from '../styles/Home.module.css'
 import styled from 'styled-components'
 
 const Location = styled.input`
-    border: 2px solid #BDFF00;
-    border-radius: 100px;
-    background: transparent;
-    padding: 7px 10px;
-    font-size: 17px;
     width: 350px;
-    font-style: italic;
-    text-align: center;
 `
 
 const Transit = styled.input`
-    border: 2px solid #BDFF00;
-    border-radius: 100px;
-    background: transparent;
-    padding: 5px 10px;
-    font-size: 17px;
     width: 100px;
-    font-style: italic;
-    text-align: center;
-`
-
-const Find = styled.button`
-    font-size: 17px;
-    background: #FF0099;
-    border: none;
-    border-radius: 100px;
-    padding: 10px;
 `
 
 const Search = () => {
@@ -50,7 +28,7 @@ const Search = () => {
             <Location type="text" placeholder="Current location" />
             Desired transit time
             <span><Transit type="value" placeholder="00" /> mins</span>
-            <Find>FIND BEATS AND BEERS</Find>
+            <button>FIND BEATS AND BEERS</button>
         </main>
     )
 }
