14b97fb8f5fbfa4271c5419f89b861c37ac1a659
stella <stella.choi@hyperisland.se>
2022-11-14 13:52:16 -0500

    fix document.ts

--- components/search/brew.tsx
+++ components/search/brew.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import styled from 'styled-components'
 import { Predictions, Prediction } from '@components/predictions/predictions'
+import { BREW } from '@utils/constants'
+import { BrewContext } from '@utils/context'
+import { useContext } from 'react'
 
-const Logo = styled.div`
-  -webkit-filter: invert(100%);
-  filter: invert(100%);
+const Logo = styled.div<{brew?: string}>`
+  -webkit-filter: ${props => props.brew === BREW.COFFEE ? 'none' : 'invert(100%)'};
+  filter: ${props => props.brew === BREW.COFFEE ? 'none' : 'invert(100%)'};
 `
 
 const Location = styled.input`
@@ -73,38 +76,42 @@ export const BrewSearch: React.FC<BrewSearchProps> = ({
   setLocated,
   locationInput,
   setUserData
-}) => (
-  <>
-    <Logo>
-      <Image src="/bike.png" alt="bike" width={180} height={95} />
-    </Logo>
-    <Location
-        type="text"
-        placeholder="Start location"
-        onChange={e => {
-          setUserData('locationInput', e.target.value)
-            setLocated(false)
-            e.target.value==='' ? setPredictions(undefined) : null
-        }}
-        value={locationInput || ''}
-    />
-    <Predictions
-        predictions={predictions && Array.isArray(predictions) ? predictions : undefined}
-        setLocated={setLocated}
-        setUserData={setUserData}
-        setPredictions={setPredictions}
-        located={located}
-    />
-    <Container>
-      <Text>Desired transit time</Text>
-      <TransitTime>
-          <Transit
-              type="number"
-              placeholder="00"
-              onChange={e => setUserData('transitTime', (parseInt(e.target.value)))}
-              onWheel={(e) => e.currentTarget.blur()}
-          /> <Min>minutes</Min>
-      </TransitTime>
-    </Container>
-  </>
-)
+}) => {
+  const brew = useContext(BrewContext)
+
+  return (
+    <>
+      <Logo brew={brew}>
+        <Image src="/bike.png" alt="bike" width={180} height={95} />
+      </Logo>
+      <Location
+          type="text"
+          placeholder="Start location"
+          onChange={e => {
+            setUserData('locationInput', e.target.value)
+              setLocated(false)
+              e.target.value==='' ? setPredictions(undefined) : null
+          }}
+          value={locationInput || ''}
+      />
+      <Predictions
+          predictions={predictions && Array.isArray(predictions) ? predictions : undefined}
+          setLocated={setLocated}
+          setUserData={setUserData}
+          setPredictions={setPredictions}
+          located={located}
+      />
+      <Container>
+        <Text>Desired transit time</Text>
+        <TransitTime>
+            <Transit
+                type="number"
+                placeholder="00"
+                onChange={e => setUserData('transitTime', (parseInt(e.target.value)))}
+                onWheel={(e) => e.currentTarget.blur()}
+            /> <Min>minutes</Min>
+        </TransitTime>
+      </Container>
+    </>
+  )
+}
