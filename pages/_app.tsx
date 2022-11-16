7d0ce530d8c401d98bbd7d6c92c0d8237cf24f5e
stella <stella.choi@hyperisland.se>
2022-11-16 14:24:24 -0500

    test import doc tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { BurgerMenu } from '@components/burgerMenu'
 import { Modal } from '@components/modal'
 import { BrewContext } from '@utils/context'
 import { getBrew } from '@utils/getBrew'
+import { Html } from 'next/dist/pages/_document'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -20,7 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   }
 
   return (
-    <>
+    <Html data-theme={brew.current}>
       <Head>
         <title>bikes, beats, and brews</title>
         <meta name="description" content="bike to brews while listening to beats" />
@@ -39,7 +40,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         
         <Component {...pageProps} modalOpen={modalOpen} setModalOpen={setModalOpen} />
       </BrewContext.Provider>
-    </>
+    </Html>
   )
 }
 
