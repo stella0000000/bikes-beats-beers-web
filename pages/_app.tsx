ec1d9bd5c794d32fd746ecf478890d002bbf4ba0
stella <stella.choi@hyperisland.se>
2022-11-21 15:36:57 -0500

    add conditional

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,24 +30,25 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/* <html data-theme={brew.current} /> */}
         <title>bikes, beats, and brews</title>
         <meta name="description" content="bike to brews while listening to beats" />
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
-      <BrewContext.Provider value={brew.current}>
-        {router.pathname !== "/" ?
-          (
-            <>
-              <BurgerMenu modalOpen={modalOpen} setModalOpen={setModalOpen} />
-              <Modal modalOpen={modalOpen} setModalOpen={setModalOpen} />
-            </>
-          )
-          : null
-        }
-        
-        <Component {...pageProps} modalOpen={modalOpen} setModalOpen={setModalOpen} />
-      </BrewContext.Provider>
+      {brew.current && 
+        (<BrewContext.Provider value={brew.current}>
+          {router.pathname !== "/" ?
+            (
+              <>
+                <BurgerMenu modalOpen={modalOpen} setModalOpen={setModalOpen} />
+                <Modal modalOpen={modalOpen} setModalOpen={setModalOpen} />
+              </>
+            )
+            : null
+          }
+          
+          <Component {...pageProps} modalOpen={modalOpen} setModalOpen={setModalOpen} />
+        </BrewContext.Provider>)
+      }
     </>
   )
 }
