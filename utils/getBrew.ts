ff545f971a2cff76b06caaf2d88a146e5dde1973
stella <stella.choi@hyperisland.se>
2022-12-20 02:08:42 -0500

    revert coffee time - was correct

--- utils/getBrew.ts
+++ utils/getBrew.ts
@@ -2,7 +2,7 @@ import { BREW } from './constants'
 
 export const getBrew = (hour: number) => {
   // between 5:00 - 15:00 coffee
-  if (5 > hour && hour < 15) {
+  if (5 < hour && hour < 15) {
     return BREW.COFFEE
   } else {
     return BREW.BEER
